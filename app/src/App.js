883375e294dfabb6a52242476d77f308b45deea7
Seungjun <hsjun999@gmail.com>
2023-03-24 13:27:07 +0400

    change unit to ether

--- app/src/App.js
+++ app/src/App.js
@@ -30,7 +30,11 @@ function App() {
     async function newContract() {
         const beneficiary = document.getElementById("beneficiary").value
         const arbiter = document.getElementById("arbiter").value
-        const value = ethers.BigNumber.from(document.getElementById("wei").value)
+        // const value = ethers.BigNumber.from(document.getElementById("wei").value)
+
+        const value = ethers.BigNumber.from(
+            ethers.utils.parseUnits(document.getElementById("ether").value.toString(), "ether")
+        )
         const escrowContract = await deploy(signer, arbiter, beneficiary, value)
 
         const escrow = {
@@ -67,8 +71,8 @@ function App() {
                 </label>
 
                 <label>
-                    Deposit Amount (in Wei)
-                    <input type="text" id="wei" />
+                    Deposit Amount (in ETHER)
+                    <input type="text" id="ether" />
                 </label>
 
                 <div
